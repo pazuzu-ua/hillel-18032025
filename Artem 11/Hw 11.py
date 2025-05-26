01bc7f3ce4545fbdb3c723bff1ddf40213a8f8a8
britka <brit.sergey@gmail.com>
2025-05-26 16:04:41 +0300

    Hw 11 done

---
+++ Artem 11/Hw 11.py
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS Cats
+(
+    cat_id INTEGER PRIMARY KEY autoincrement,
+name   TEXT NOT NULL,
+weight INT CHECK (weight > 0)
+);
+
+insert into Cats(name, weight)
+values ('Tuzik', 15),
+('Zevs', 23),
+('Mars', 10),
+('Busya', 13),
+('Dusya', 19);
+
+SELECT *
+From Cats
+where weight >= 10
+and weight <= 15
+group by weight;
